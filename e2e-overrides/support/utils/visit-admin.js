470c7a5e8671312d55fd779ac4fcf909491be733
Copons <copons@users.noreply.github.com>
2018-11-21 13:31:06 +0000

    Update to Gutenberg v4.4.0

---
+++ e2e-overrides/support/utils/visit-admin.js
@@ -0,0 +1,20 @@
+/** @format */
+/**
+ * Node dependencies
+ */
+import { join } from 'path';
+
+/**
+ * Internal dependencies
+ */
+import { goToWPPath } from './go-to-wp-path';
+import { login } from './login';
+
+export async function visitAdmin(adminPath, query) {
+	await goToWPPath(join('wp-admin', adminPath), query);
+
+	if (page.url().indexOf('/log-in') !== -1) {
+		await login();
+		return visitAdmin(adminPath, query);
+	}
+}
