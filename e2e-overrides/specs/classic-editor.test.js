1a0d9f8d3ff4f03160316cb8de81a6eb80952499
Copons <copons@users.noreply.github.com>
2018-11-22 15:51:36 +0000

    Disable classic-editor spec

---
+++ e2e-overrides/specs/classic-editor.test.js
@@ -0,0 +1,28 @@
+/**
+ * Internal dependencies
+ *
+ * @format
+ */
+
+import { visitAdmin } from '../support/utils';
+
+xdescribe('classic editor', () => {
+	beforeAll(async () => {
+		await visitAdmin('post-new.php', 'classic-editor');
+	});
+
+	it('Should work properly', async () => {
+		// Click visual editor
+		await expect(page).toClick('#content-tmce');
+		await expect(page).toClick('#content_ifr');
+
+		// type some random text
+		await page.keyboard.type('Typing in classic editor');
+
+		// Switch to HTML mode
+		await expect(page).toClick('#content-html');
+
+		const textEditorContent = await page.$eval('.wp-editor-area', element => element.value);
+		expect(textEditorContent).toEqual('Typing in classic editor');
+	});
+});
